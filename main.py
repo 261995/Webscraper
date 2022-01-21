c203fb5d275d4b790df99053a7fa331c6aef61ef
261995 <261995@student.pwr.edu.pl>
2022-01-21 04:04:10 +0100

    21.01.2022 04:04

--- main.py
+++ main.py
@@ -33,36 +33,85 @@ if __name__ == '__main__':
     main()
 
 # Scrape url
-def parse_from_page():
+def parse_from_page1():
 
-    pageURL = 'https://www.zalando-lounge.pl/campaigns/ZZO1M4A/1'
+    page1URL = 'https://www.zalando-lounge.pl/campaigns/ZZO1M4A/1'
 
 
-    page = requests.get(pageURL)  # zapytanie pobrania zawartości strony
+    page = requests.get(page1URL)  # zapytanie pobrania zawartości strony
     bs = BeautifulSoup(page.text, 'html.parser')  # pobranie zawartości zapytania
     # print (bs)
 
 
-    title = bs.find_all("div", class_="styles__StyledText-cia9rt-0 zUiGX").get_text().strip().split(' - ').[1]
+    title = bs.find_all("div", class_="styles__StyledText-cia9rt-0 zUiGX").get_text().strip().split(' - ')
     firm = bs.find_all("span", class_="articleName___uppercase___byJM8").get_text().strip()
-    color = bs.find_all("span", class_="styles__StyledText-cia9rt-0 zUiGX").get_text().strip().split(' - ').[2]
+    color = bs.find_all("span", class_="styles__StyledText-cia9rt-0 zUiGX").get_text().strip().split(' - ')
     size = bs.find_all("span", class_="styles__StyledText-cia9rt-0 ljhdMk").get_text()
-    price = bs.find_all("span", class_="styles__StyledText-cia9rt-0 ixsQUL").get_text().strip().split('&').[1]
+    price = bs.find_all("span", class_="styles__StyledText-cia9rt-0 ixsQUL").get_text().strip().split('&')
 
-    # cursor.execute('INSERT INTO article VALUES (?, ?, ?)', (title, firm, color, price))
+    cursor.execute('INSERT INTO article VALUES (?, ?, ?)', (title, firm, color, size, price))
     # wpisanie zmiennych do wartości
 
-    print("Title:")
-    print(title)
-    print ('koniec parse')
+    # print(title)
+    # print ('koniec parse')
 
-    # db.commit()
+    db.commit()
+
+def parse_from_page2():
+
+    page2URL = 'https://www.zalando-lounge.pl/campaigns/ZZO1KHB/1'
+
+
+    page = requests.get(page2URL)  # zapytanie pobrania zawartości strony
+    bs = BeautifulSoup(page.text, 'html.parser')  # pobranie zawartości zapytania
+    # print (bs)
+
+
+    title = bs.find_all("div", class_="styles__StyledText-cia9rt-0 zUiGX").get_text().strip().split(' - ')
+    firm = bs.find_all("span", class_="articleName___uppercase___byJM8").get_text().strip()
+    color = bs.find_all("span", class_="styles__StyledText-cia9rt-0 zUiGX").get_text().strip().split(' - ')
+    size = bs.find_all("span", class_="styles__StyledText-cia9rt-0 ljhdMk").get_text()
+    price = bs.find_all("span", class_="styles__StyledText-cia9rt-0 ixsQUL").get_text().strip().split('&')
+
+    cursor.execute('INSERT INTO article VALUES (?, ?, ?)', (title, firm, color, size, price))
+    # wpisanie zmiennych do wartości
+
+    # print(title)
+    # print ('koniec parse')
+
+    db.commit()
+
+def parse_from_page3():
+
+    page3URL = 'https://www.zalando-lounge.pl/campaigns/ZZO1MAY/1'
+
+
+    page = requests.get(page3URL)  # zapytanie pobrania zawartości strony
+    bs = BeautifulSoup(page.text, 'html.parser')  # pobranie zawartości zapytania
+    # print (bs)
+
+
+    title = bs.find_all("div", class_="styles__StyledText-cia9rt-0 zUiGX").get_text().strip().split(' - ')
+    firm = bs.find_all("span", class_="articleName___uppercase___byJM8").get_text().strip()
+    color = bs.find_all("span", class_="styles__StyledText-cia9rt-0 zUiGX").get_text().strip().split(' - ')
+    size = bs.find_all("span", class_="styles__StyledText-cia9rt-0 ljhdMk").get_text()
+    price = bs.find_all("span", class_="styles__StyledText-cia9rt-0 ixsQUL").get_text().strip().split('&')
+
+    cursor.execute('INSERT INTO article VALUES (?, ?, ?)', (title, firm, color, size, price))
+    # wpisanie zmiennych do wartości
+
+    # print(title)
+    # print ('koniec parse')
+
+    db.commit()
 
 if len(argv) > 1 and argv[1] == 'setup':
-    cursor.execute('''CREATE TABLE article (Item TEXT, Firm TEXT, Color TEXT, Price REAL)''')
+    cursor.execute('''CREATE TABLE article (Item TEXT, Firm TEXT, Color TEXT, Size TEXT, Price REAL)''')
     # tworzenie tabeli zawierającej informacje o produktach
     quit()
 
-parse_from_page()
+parse_from_page1()
+parse_from_page2()
+parse_from_page3()
 
 db.close()
